2455fe6408d13670841da8ae5a6aaf732d092b75
Patrick Petrushonis <pwp904@gmail.com>
2018-06-12 21:42:44 -0700

    Added owl.carousel plugin for learn about section with schools, skills, and passives content.

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,8 +1,10 @@
 
 // Bower components
 // =require ../bower_components/jquery/dist/jquery.min.js
+// =require ../bower_components/owl.carousel/dist/owl.carousel.min.js
 
 // Custom JavaScript partials
+// =require partials/carousel.js
 // =require partials/footer.js
 // =require partials/header.js
 // =require partials/modal.js
