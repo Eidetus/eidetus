98a5e0275de4fa038cabd7b87bf2093008f66cdc
Patrick Petrushonis <pwp904@gmail.com>
2018-06-15 22:58:08 -0700

    Consolidated 'bower_components' into 'node_modules'.

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,7 +1,7 @@
 
-// Bower components
-// =require ../bower_components/jquery/dist/jquery.min.js
-// =require ../bower_components/owl.carousel/dist/owl.carousel.min.js
+// Node Modules
+// =require ../node_modules/jquery/dist/jquery.min.js
+// =require ../node_modules/owl.carousel/dist/owl.carousel.min.js
 
 // Custom JavaScript partials
 // =require partials/carousel.js
