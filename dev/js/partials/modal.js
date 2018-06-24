bbd7597e3633d775aded451f26d1e6fdeb4f1978
Patrick Petrushonis <pwp904@gmail.com>
2018-06-24 13:21:33 -0700

    Added trailer modal in preparation for trailer release.

--- dev/js/partials/modal.js
+++ dev/js/partials/modal.js
@@ -6,12 +6,25 @@ var newsletterModal = new tingle.modal({
     beforeClose: function() { return true; },
     cssClass: []
 });
+var trailerModal = new tingle.modal({
+    onClose: function() {},
+    onOpen: function() {},
+    beforeOpen: function() {},
+    beforeClose: function() { return true; },
+    cssClass: ['tingle-modal--no-padding']
+});
 
-var btn = document.querySelector('[modal-ID="newsletter-modal"]');
+var btn1 = document.querySelector('[modal-ID="newsletter-modal"]');
+var btn2 = document.querySelector('[modal-ID="trailer-modal"]');
 
 // Event listener that opens modal with associated content based on data attributes
-btn.addEventListener('click', function() {
+btn1.addEventListener('click', function() {
     newsletterModal.open();
     var content = document.querySelector('[modal-content="newsletter-modal"]');
     newsletterModal.setContent(content ? content.innerHTML : '<h2>Unable to load content</h2>');
-});
+});/*
+btn2.addEventListener('click', function() {
+    trailerModal.open();
+    var content = document.querySelector('[modal-content="trailer-modal"]');
+    trailerModal.setContent(content ? content.innerHTML : '<h2>Unable to load content</h2>');
+});*/
