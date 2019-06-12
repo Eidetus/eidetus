e0a9087212059b3a3d1da596898c02eb64031d5b
Patrick Petrushonis <pwp904@gmail.com>
2019-06-12 07:08:21 -0700

    Fixed issue causing trailer modal to continue playing when closed.

--- dev/js/partials/modal.js
+++ dev/js/partials/modal.js
@@ -8,7 +8,16 @@ document.querySelectorAll('[modal-ID]').forEach(function(button) {
     // Event listener that opens modal with associated content based on data attributes
     button.addEventListener('click', function() {
         var modal = new tingle.modal({
-            onClose: function() {},
+            onClose: function() {
+                // Ensure trailer modal is removed from DOM
+                if (this.classList.contains("tingle-modal--video")) {
+                    modalElement = document.querySelector('.tingle-modal--video');
+
+                    if (modalElement) {
+                        modalElement.parentNode.removeChild(modalElement);
+                    }
+                }                
+            },
             onOpen: function() {},
             beforeOpen: function() {},
             beforeClose: function() { return true; },
